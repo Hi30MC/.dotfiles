d44dc977c9a3ede178933196f60e9dbf4dd48ef1
HiHi3690 <duimstrajakson@gmail.com>
2025-04-15 00:12:03 -0400

    added packages to install.sh, made it executable added F-Sy-H zsh script

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd paru
 makepkg -si
 
 # install base packages
-paru -S zsh firefox discord kde-connect code
+paru -S zsh firefox discord kde-connect alacritty starship
 
 # install vencord
 sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
@@ -17,7 +17,14 @@ sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/
 # install openasar
 sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
 
+# switch to zsh
+chsh -s $(which zsh)
+
 # install zsh scripts
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git  ~/.local/share/zsh/scripts/F-Sy-H
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.local/share/zsh/scripts/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions.git ~/.local/share/zsh/scripts/zsh-completions
+
+# install secondary packages
+
+paru -S code cloudflare-warp-bin
