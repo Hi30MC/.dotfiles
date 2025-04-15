5353e1bb8d7b3e9675f2213e946e5ae921e3e205
HiHi3690 <duimstrajakson@gmail.com>
2025-04-14 23:32:17 -0400

    cleaned up .minecraft/config/schematics/ and updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+#yes I know the way this is made is super non-standard, fight me
+
+# install paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# install base packages
+paru -S zsh firefox discord kde-connect code
+
+# install vencord
+sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+
+# install openasar
+sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+
+# install zsh scripts
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git  ~/.local/share/zsh/scripts/F-Sy-H
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.local/share/zsh/scripts/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions.git ~/.local/share/zsh/scripts/zsh-completions
